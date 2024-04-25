a0d7c6dcc0e829ec4993c6f7fd4b32099a8d361a
kevinnadr <kevin19305.ib@gmail.com>
2024-04-25 19:04:22 +0700

    bool serachNode new

--- node.cpp
+++ node.cpp
@@ -2,61 +2,73 @@
 using namespace std;
 
 struct Node {
-    int noMhs;
-    string name;
-    Node* next;
+	int noMhs;
+	string name;
+	Node* next;
 };
 
 Node* START = NULL;
 
-void addNode(){
-    int nim;
-    string nama;
-    Node* nodeBaru =new Node();
-    cout << "Masukan NIM: ";
-    cin >> nim;
-    cout << "Masukan Nama: ";
-    cin >> nama;
-    nodeBaru->noMhs = nim;
-    nodeBaru->name = nama;
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
 
-    if (START == NULL || nim <= START ->noMhs) {
-        if (START != NULL && nim == START ->noMhs)
-        {
-            cout << "NIM sudah ada" << endl;
-            return;
-        }
-         nodeBaru->next = START;
-         START = nodeBaru;
-         return;
-        
-    }
-    
-    Node* previous = START;
-    Node* current = START;
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
 
-    while ((current != NULL) && (nim >= current ->noMhs))
-    {
-        if (nim == current->noMhs)
-        {
-            cout << "NIM sudah ada" << endl;
-            return;
-        }
-        previous = current;
-        current = current->next;
-    }
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
 
-    nodeBaru->next = current;
-    previous->next = nodeBaru;
+	Node* previous = START;
+	Node* current = START;
+	
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
 
 bool serachNode(int nim, Node* current, Node* previous) {
-    previous = START;
-    current = START;
-    while (current != NULL && nim > current->noMhs)
-    {
-        previous = current;
-        current = current->next;
-    }
-    
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
